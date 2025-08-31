2420d60cde58ad14634df7293fa60202daffc072
dbrogen <daniel.brogen1@marist.edu>
2025-08-31 16:34:39 -0400

    Created Bare Bones Conveyor Belt Infrastructure
    Overview
    - Created a 3D scene with a conveyor belt
    
    Details
    - Grey boxed an environment with a cube and 3 planes to make a very basic conveyor belt
    - created two scripts: BeltBehavior and Node
        - BeltBehavior operates the conveyor belt behavior
        - Node is an empty script that marks the nodes on the path for the box to follow

---
+++ 00 Unity Proj/Package/Assets/Scripts/BeltBehavior.cs
@@ -0,0 +1,81 @@
+using System.Threading;
+using UnityEngine;
+
+/*
+ * 
+ * Variables:
+ * - box: contains the current gameobject for a stand in box
+ * - pathNodes: contains an array of nodes for the box to follow
+ * - moveSpeed: how fast the box moves along the path
+ * - currentPosition: the position of the currentNode
+ * - currentNode: the currently accessed node in the pathNodes array
+ * - boxSpeed: overall speed of the box while following the path
+ * 
+ */
+
+public class BeltBehavior : MonoBehaviour
+{
+
+    public GameObject box;
+
+    public Node[] pathNodes;
+
+    public float moveSpeed = 10f;
+
+    static Vector3 currentPosition;
+
+    private int currentNode;
+
+    private float boxSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        //Fills the array with the current node children of the path
+        pathNodes = GetComponentsInChildren<Node>();
+
+        CheckNode();
+
+    }
+
+    void CheckNode()
+    {
+
+        //reset speed temporarily to 0 and assign new node position to current position
+        boxSpeed = 0;
+        currentPosition = pathNodes[currentNode].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //boxSpeed is assigned to the time between frame updates and moveSpeed
+        boxSpeed = Time.deltaTime * moveSpeed;
+
+        //if the box position is not at the current goal node position the box moves closer towards the goal node position
+        //if it is the current goal node position the currentNode variable increments and CheckNode() gets called to update
+        //the next goal node position
+
+        if(box.transform.position != currentPosition)
+        {
+
+            box.transform.position = Vector3.MoveTowards(box.transform.position, currentPosition, boxSpeed);
+
+        }
+        else 
+        {
+            if(currentNode <  pathNodes.Length - 1)
+            {
+                
+                currentNode++;
+                CheckNode();
+
+            }
+        
+        }
+
+    }
+}

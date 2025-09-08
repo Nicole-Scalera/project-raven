ee93ee2d596666ec97bc9d8d8eb0751302734210
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-09-08 17:20:51 -0400

    Updated SceneChanger2.cs
    Overview
    - Updated SceneChanger2.cs.
    - This commit is related to #14.
    
    In-depth Details
    - I will break down the information about this commit down below.
    
    A. Two SceneChangers
    - In 829779b, I added a bunch of scripts from a repo called Unity-SerializableDictionary by @azixMcAze.
         - This is because dictionaries are not serializable in the Unity editor by default.
    - Many of these scripts I edited to fit this project's purposes, including UserSerializableDictionaries.cs.
    - Moreover, this commit introduces a bunch of changes to the new scene changer feature I have discussed in #14.
    - I will rename the file later, but wanted to still keep the current SceneChanger.cs, which is still in use in SC_Level2.unity (67ec0ad).
    
    B. Code Updates
    - Inside of SceneChanger2.cs, you'll notice that I added some comments dividing up the code into logical parts.
    
         1. "Initialization"
              - Contains basic methods that are called at runtime.
              - Ex: assigning the GameManager; getting the dictionary contents for GameObjects and SceneAssets.
    
         2. "SceneChanger Methods"
              - Dynamic methods that are used universally in every scene-changing context.
              - Ex: LoadScene() works regardless of what triggered it.
    
         3. "Event Methods"
              - Here are specified events that trigger one of the aforementioned SceneChanger Methods.
              - Specifically, this includes:
                   1. OnCollisionEnter() for collision with 3D objects
                   2. CheckForClicks() and TaskOnClick() for 2D UI elements
              - Although I am still ironing out the kinks, you'll notice that both scenarios can use the CheckForKey() and GetDictionaryScene() methods.
    
         4. "Dictionary Logic"
              - These methods are specifically made to handle logic related to the given object's sceneDictionary.
              - The sceneDictionary is based on the "locations" dictionary that was originally used in PlayerCollision.
              	- Its functioning is explained further in #14.
              - Just like the SceneChanger Methods, these functions can be used for both 2D and 3D scenarios.
    
    C. Goals
    - In my next steps, I am planning to branch out into a 2D scene switcher and a 3D scene switcher file.
    - Theoretically, these subclasses would utilize the interfaces specified in the SceneChanger2.cs file.
    - I am still reworking them and identifying what methods they need, but this feature is on the up-and-up.

--- 00 Unity Proj/Package/Assets/core features/SceneSwitching/SceneChanger2.cs
+++ 00 Unity Proj/Package/Assets/core features/SceneSwitching/SceneChanger2.cs
@@ -1,121 +1,166 @@
-// using UnityCommunity.UnitySingleton;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-//
-// // This is the SceneChanger class. When called, it will change
-// // the current scene to a new one of a specific name. Set the
-// // name in the Inspector, or create a line to manually assign
-// // it through the code.
-//
-// namespace SceneSwitching_cf
-// {
-//     public abstract class SceneChanger2 : MonoSingleton<SceneChanger>, ICollidable, ISceneChanger
-//     {
-//
-//         // ===== Variables/Components =====
-//         private GameManager gameManager; // Access GameManager.cs
-//         protected Scene scene;  // drag your scene here
-//         protected string sceneName; // Name of a scene
-//         private Rigidbody rb; // Rigidbody
-//         protected string Button; // UI Buttons (assigned in Inspector)
-//         protected GameObject otherObject; // Other object player collides with
-//         // ================================
-//
-//         protected virtual void Awake()
-//         {
-//             gameManager = GameManager.Instance; // Assign GameManager.cs
-//         }
-//
-//         // Load a scene by name (set name in Inspector)
-//         public virtual void LoadScene(Scene scene)
-//         {
-//             // Get the scene asset from the editor and
-//             // convert it to a string
-//             this.scene = scene;
-//             sceneName = scene.name;
-//             
-//             // Tell the user what scene is being loaded
-//             Debug.Log($"SceneChanger > Loading scene: {sceneName}...");
-//
-//             // Load a scene by its name
-//             SceneManager.LoadScene(sceneName);
-//
-//             //  Confirm the scene was loaded
-//             Debug.Log($"SceneChanger > Loaded successfully!");
-//         }
-//
-//         // Get the current scene
-//         public virtual string GetCurrentSceneName()
-//         {
-//             // Grab the scene by its name
-//             return SceneManager.GetActiveScene().name;
-//         }
-//         
-//         // In a 3D scene, we have attached this script to the Player.
-//         public virtual void OnCollisionEnter(Collision other)
-//         {
-//             FindCollisionScene();
-//             
-//             if (other.gameObject.CompareTag(tag))
-//             {
-//                 otherObject = other.gameObject;
-//             }
-//             
-//             otherObject = other.gameObject;
-//             this.scene = scene;
-//         }
-//         
-//         public virtual Scene FindCollisionScene(GameObject otherObject)
-//         {
-//             
-//         }
-//         
-//         //, GameObject otherObject, TagHandle tag, Scene scene
-//         
-//         //public virtual void OnCollisionEnter(Collision other)
-//         //{
-//             // This method is intentionally left blank.
-//             // Implement collision handling in derived classes.
-//         //}
-//         
-//
-//         // Call sceneLoaded when a new scene is loaded
-//         public virtual void OnEnable()
-//         {
-//             SceneManager.sceneLoaded += OnSceneLoaded;
-//         }
-//
-//         public virtual void OnDisable()
-//         {
-//             SceneManager.sceneLoaded -= OnSceneLoaded;
-//         }
-//
-//         // Notify the GameManager of the new "current" scene name
-//         public virtual void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-//         {
-//             // GameManager always prints whenever a
-//             // new scene is loaded, included runtime.
-//             gameManager.UpdateCurrentScene(scene.name);
-//         }
-//         
-//     }
-//     
-//         // This interface is used to handle UI events for scene changing
-//         public interface ISceneChanger
-//         {
-//             void Awake();
-//             void LoadScene(Scene scene);
-//             void GetCurrentSceneName();
-//             void OnEnable();
-//             void OnDisable();
-//             void OnSceneLoaded(Scene scene, LoadSceneMode mode);
-//     
-//         }
-//     
-//     // This interface is used to handle collision events
-//     public interface ICollidable
-//     {
-//         void OnCollisionEnter(Collision other);
-//     }
-//
-// }
+using System.Collections.Generic;
+using UnityCommunity.UnitySingleton;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This is the SceneChanger class. When called, it will change
+// the current scene to a new one of a specific name. Set the
+// name in the Inspector, or create a line to manually assign
+// it through the code.
+
+namespace SceneSwitching_cf
+{
+    public abstract class SceneChanger2 : MonoSingleton<SceneChanger>, ISceneChanger, ICollidable 
+    {
+
+        // ===== Variables/Components =====
+        private GameManager gameManager; // Access GameManager.cs
+        protected SceneAsset scene;  // The SceneAsset in the Editor
+        protected string sceneName; // Name of a scene
+        private Rigidbody rb; // Rigidbody
+        protected string Button; // UI Buttons (assigned in Inspector)
+        protected GameObject otherObject; // Other object player collides with
+        protected SC_CollisionRoster sceneDictionary; // Reference to the collision dictionary
+        // ================================
+
+        // ===== Initialization =====
+        public virtual void Awake()
+        {
+            // Assign GameManager.cs
+            gameManager = GameManager.Instance;
+        }
+        
+        protected virtual void Start()
+        {
+            // Initialize the scene changer Dictionary
+            GetDictionary();
+            CheckForClicks();
+        }
+        
+        // Get the scene dictionary from SceneChangeDictionary.cs
+        public virtual void GetDictionary()
+        {
+            sceneDictionary = GetComponent<SC_CollisionRoster>();
+        }
+        // ==========================
+
+        // ================ SceneChanger Methods =================
+        // Load a scene by name
+        public virtual void LoadScene(string sceneName)
+        {
+            // Tell the user what scene is being loaded
+            Debug.Log($"SceneChanger > Loading scene: {sceneName}...");
+
+            // Load a scene by its name
+            SceneManager.LoadScene(sceneName);
+
+            //  Confirm the scene was loaded
+            Debug.Log($"SceneChanger > Loaded successfully!");
+        }
+        
+        // Return a string of the current scene
+        public string GetCurrentSceneName()
+        {
+            // Grab the scene by its name
+            return SceneManager.GetActiveScene().name;
+        }
+        
+        // Call sceneLoaded when a new scene is loaded
+        public virtual void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public virtual void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        // Notify the GameManager of the new "current" scene name
+        public virtual void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            // GameManager always prints whenever a
+            // new scene is loaded, included runtime.
+            gameManager.UpdateCurrentScene(scene.name);
+        }
+        // =======================================================
+        
+        // =================== Event Methods ===================
+        // Checking for collisions in a 3D environment
+        public virtual void OnCollisionEnter(Collision other)
+        {
+            // Set the otherObject to the UI/collided object
+            otherObject = other.gameObject;
+            
+            // Call CheckForKey to see if it is in the dictionary
+            CheckForKey(otherObject);
+        }
+        
+        // Check for button clicks in the scene
+        void CheckForClicks()
+        {
+            // Handle logic in a derived class
+        }
+
+        void TaskOnClick(string buttonName)
+        {
+            // Handle logic in a derived class
+        }
+        // =====================================================
+        
+        // ================== Dictionary Logic ==================
+        // Call this in OnCollisionEnter or CheckForClicks
+        public virtual void CheckForKey(GameObject other)
+        {
+            // Check if the object the Player interacted/collided
+            // with is specified in the scene changer dictionary
+            if (sceneDictionary.ContainsKey(otherObject))
+            {
+                // Grab the scene associated with the object
+                GetDictionaryScene(otherObject);
+                
+                // Convert the sceneName to a string
+                sceneName = scene.name;
+                Debug.Log("sceneName: " + sceneName);
+                
+                // Load the scene by its name
+                LoadScene(sceneName);
+            }
+            else
+            {
+                // Otherwise, throw out the value
+                otherObject = null;
+            }
+        }
+        
+        // Get the associated scene from sceneDictionary
+        public virtual void GetDictionaryScene(GameObject otherObject)
+        {
+            Debug.Log($"SceneChanger > Getting scene dictionary: {sceneName}...");
+            // Assign the scene from the dictionary
+            scene = sceneDictionary[otherObject];
+            Debug.Log($"SceneChanger > {sceneName} retrieved.");
+        }
+        // ======================================================
+        
+    }
+    
+    // This interface is used to handle UI events for scene changing
+    public interface ISceneChanger
+    {
+        void Awake();
+        void LoadScene(string sceneName);
+        string GetCurrentSceneName();
+        void OnEnable();
+        void OnDisable();
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode);
+    }
+    
+    // This interface is used to handle collision events
+    public interface ICollidable
+    {
+        void OnCollisionEnter(Collision other);
+    }
+
+}

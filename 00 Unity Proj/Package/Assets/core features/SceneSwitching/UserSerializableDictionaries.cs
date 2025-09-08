829779b7c2cdb275c0bc4e2f1fe1d1f5f0a49b18
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-09-08 15:21:13 -0400

    Started the New Scene Changer Feature
    Overview
    - This commit is related to #14.
    - Many scripts have been added and thus this snapshot might not be a perfectly stable version.
    
    In-depth Details
    - As explained in #14, I am in the works of implementing a new dynamic scene changer that utilizes serializable dictionaries in the Unity inspector.
    - Because dictionaries are not serializable by default, I am using scripts from a repo called Unity-SerializableDictionary by @azixMcAze.
    - I am currently uploading some scripts from that repo, plus scripts I edited from there to create a custom Script component in the Inspector that has dictionary items for "GameObject" : "SceneAsset"
    - Again, this is a WIP, so it's definitely not complete.
    - See #14 for the details on this idea.

---
+++ 00 Unity Proj/Package/Assets/core features/SceneSwitching/UserSerializableDictionaries.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace SceneSwitching_cf
+{
+
+// Default "string" : "string" dictionary
+    [Serializable]
+    public class StringStringDictionary : SerializableDictionary<string, string>
+    {
+    }
+
+// Dictionary for linking specfic UI elements with specific
+// Scene Assets, with respect to button-based scene changes.
+    [Serializable]
+    public class SC_UIRoster : SerializableDictionary<GameObject, SceneAsset>
+    {
+    }
+
+// Dictionary for linking specific Game Objects with specific
+// Scene Assets, with respect to collision-based scene changes. 
+    [Serializable]
+    public class SC_CollisionRoster : SerializableDictionary<GameObject, SceneAsset>
+    {
+    }
+
+}

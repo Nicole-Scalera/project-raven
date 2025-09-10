754ed2422d8a4ae8e8e27dc92984f6bb2232fec9
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-09-10 11:34:30 -0400

    Created SceneChanger_UI.cs & SceneChanger_Collision.cs
    Overview
    - Updated SceneChanger2.cs.
    - Created SceneChanger_UI.cs and SceneChanger_Collision.cs.
    
    In-depth Details
    - This commit is related to the new scene-switcher plan described in #14 & #15.
    - SceneChanger2.cs is just about ready for final use, I just updated it to have the GetDictionary() method be overridden in the child classes.
         - This is for the separate dictionary types created in 829779b.
    - As for the new files, they inherit from SceneChanger2.cs for specific purposes:
         - UI-based scene switching
         - Collision-based scene switching
    - Because they share most of their logic, they are both inheriting from SceneChanger2.cs.
    - However, they utilize interfaces (e28bf23) to decouple the joint files when needed.
         - e.g., the UI-based scene changer doesn't need an OnCollision() method.

---
+++ 00 Unity Proj/Package/Assets/core features/SceneSwitching/SceneChanger_UI.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityCommunity.UnitySingleton;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This is the SceneChanger_UI class, which inherits from SceneChanger2.cs.
+// It is designed to handle scene changes triggered by UI interactions,
+// such as button clicks. See SceneChanger_Collision.cs for collision-based
+// scene changes.
+
+namespace SceneSwitching_cf {
+    public class SceneChanger_UI : SceneChanger2, ISceneChanger, IClickable
+    {
+        // ===== Variables/Components =====
+        protected SC_UIRoster sceneDictionary; // UI dictionary
+        // ================================
+        
+        // Get the scene dictionary from SceneChangeDictionary.cs
+        public virtual void GetDictionary()
+        {
+            sceneDictionary = GetComponent<SC_UIRoster>();
+        }
+        
+        // =================== Event Methods ===================
+        // Check for button clicks in the scene
+        public virtual void CheckForClicks()
+        {
+            // Handle logic in a derived class
+        }
+
+        public virtual void TaskOnClick(string buttonName)
+        {
+            // Handle logic in a derived class
+        }
+        // =====================================================
+    }
+}

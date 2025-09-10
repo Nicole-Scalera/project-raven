2b6ddd7b554782c7af07de57e333aa7a07897a00
dbrogen <daniel.brogen1@marist.edu>
2025-09-10 11:28:28 -0400

    Renamed Parent Folder
    Overview:
    - Renamed the folder for the conveyor belt scripts to match the standardized folder naming convention

---
+++ 00 Unity Proj/Package/Assets/Conveyor Belt Scripts/ObjectManager.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityCommunity.UnitySingleton;
+
+/*
+ * 
+ * Variables:
+ * - boxPrefabs: contains the prefabs for each box
+ * - timer: manual timer for spawnwing boxes
+ * - maxTime: max amount of time for the timer in seconds
+ * 
+ */
+
+
+public class ObjectManager : MonoBehaviour
+{
+
+    private Vector3 spawnPosition = new Vector3(20.0f, 5.5f, 0.0f);
+
+    [Header("Prefabs")]
+    public GameObject[] boxPrefabs;
+
+    [Header("Box Spawning")]
+    public float timer = 0;
+    public float maxTime = 5;
+
+    [Header("Pathing")]
+    public GameObject path;
+
+    private void Update()
+    {
+        // if the timer is not at its maxTime increment the timer
+        // else create a new box and reset the timer
+        if(timer < maxTime)
+        {
+
+            timer += Time.deltaTime;
+
+        }
+        else 
+        {
+
+            //TODO: figure out a way to add a tag to the instantiate
+
+            GameObject newBox = Instantiate(boxPrefabs[0], spawnPosition, Quaternion.identity);
+
+            path.GetComponent<BeltBehavior>().AddBox(newBox);
+
+            timer = 0;
+        
+        }
+        
+    }
+
+}

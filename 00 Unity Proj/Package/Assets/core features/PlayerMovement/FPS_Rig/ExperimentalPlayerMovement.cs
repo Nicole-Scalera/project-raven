9a0abc520ca89362b9dbbf4adea5d76d9569e495
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-09-29 00:05:17 -0400

    WIP | Camera Following Player
    Overview
    - Updated FPS_Rig.unity.
    - This commit is apart of #23.
    
    In-depth Details
    - The camera is now following the player with the new changes that were just made to ExperimentalPlayerMovement.cs in b86d8a1.
    - I am struggling to get the camera to move when the player jumps.
    - Also still tweaking the in-line equation for the camera rotation.

--- 00 Unity Proj/Package/Assets/core features/PlayerMovement/FPS_Rig/ExperimentalPlayerMovement.cs
+++ 00 Unity Proj/Package/Assets/core features/PlayerMovement/FPS_Rig/ExperimentalPlayerMovement.cs
@@ -15,6 +15,7 @@ namespace FPS_Rig_cf
         public Rigidbody rb; // Rigidbody
         [SerializeField] public PlayerControls playerControls;
         private Player player; // Player.cs
+        public GameObject camera; // Main Camera
         // ================================
 
         // ========== Basic Movement ==========
@@ -22,6 +23,11 @@ namespace FPS_Rig_cf
         public float moveSpeed = 5.0f;
         public float xMovement; //left and right
         public float zMovement; //forward and back
+
+        [Header("Orientation Variables")]
+        public Vector2 lookDirection;
+        private float rotationX;
+        private float rotationY;
         // ====================================
 
         // ========== Jumping ==========
@@ -65,6 +71,9 @@ namespace FPS_Rig_cf
         {
             // Updates linearVelocity with new (inputted) values
             rb.linearVelocity = new Vector3(xMovement * moveSpeed, rb.linearVelocity.y, zMovement * moveSpeed);
+
+            // Adjust the camera angle
+            // transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
             
             // Check if the player is on the ground
             GroundCheck();
@@ -78,18 +87,24 @@ namespace FPS_Rig_cf
         {
             // Prints the input action and its details
             // Debug.Log(context.action.ToString());
+            camera.transform.position = new Vector3(transform.position.x, player.transform.position.y + 3.17f, transform.position.z);
+            camera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
 
             // Reads the X (left/right) and Z (forward/back) variables from the
             // Vector3 & assigns them to the corresponding movement variables
             xMovement = playerControls.PlayerMove.Move.ReadValue<Vector3>().x;
             zMovement = playerControls.PlayerMove.Move.ReadValue<Vector3>().z;
             jumpMovement = playerControls.PlayerMove.Move.ReadValue<Vector3>().y;
+            
+            rotationX = playerControls.PlayerMove.Look.ReadValue<Vector2>().x;
+            rotationY = playerControls.PlayerMove.Look.ReadValue<Vector2>().y;
 
             if (isGrounded && jumpsRemaining > 0)
             {
                 if (jumpMovement != 0)
                 {
                     rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpPower, rb.linearVelocity.z);
+                    camera.transform.position = new Vector3(transform.position.x, player.transform.position.y + 3.17f, transform.position.z);
                     jumpsRemaining--;
                 }
             }

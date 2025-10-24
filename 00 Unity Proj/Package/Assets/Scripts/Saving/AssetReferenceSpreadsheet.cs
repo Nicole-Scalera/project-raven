27721fe1f328060fdab5a3cc20fadbf7652dbaaf
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-10-24 11:36:07 -0400

    Clue Spreadsheet Saves to File
    Overview
    - Established basic saving of a CSV file to the player prefs save folder for Unity.
    - This commit is related to #44.
    
    In-depth Details
    - In 55133ec, I created a functional addressable asset for the spreadsheet of clue data.
         - Not real clue data at the moment, just contains examples.
    - Once the data is retrieved from the cloud, it is downloaded to my machine and saved to an ES3Spreadsheet.
    - I saved it as a file named myClueData.csv.
    - It won't appear in this commit because the .gitignore probably ignores player pref files.
    - I am working on the formatting in which it is saved, but this is headed in the right direction.

--- 00 Unity Proj/Package/Assets/Scripts/Saving/AssetReferenceSpreadsheet.cs
+++ 00 Unity Proj/Package/Assets/Scripts/Saving/AssetReferenceSpreadsheet.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+
+// This class allows you to create an Addressable Asset Reference
+// for TextAsset spreadsheets. It was created since ES3Spreadsheet
+// works with TextAssets for loading and saving data.
+
 [System.Serializable]
 public class AssetReferenceSpreadsheet : AssetReferenceT<TextAsset>
 {
--- 00 Unity Proj/Package/Assets/Scripts/Saving/ClueSavingTest.cs
+++ 00 Unity Proj/Package/Assets/Scripts/Saving/ClueSavingTest.cs
@@ -8,17 +8,43 @@ public class ClueSavingTest : MonoBehaviour
 
     private void Start()
     {
+        // Load the clueSpreadsheet asynchronously
+        // and register a callback for when it's loaded
         clueSpreadsheet.LoadAssetAsync().Completed += OnClueSpreadsheetLoaded;
     }
 
+    // When clueSpreadsheet is loaded, load the data into an ES3Spreadsheet
     public void OnClueSpreadsheetLoaded(AsyncOperationHandle<TextAsset> handle)
     {
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             TextAsset clueData = handle.Result;
+            // string assetPath = handle.Location?.InternalId ?? "unknown";
             var sheet = new ES3Spreadsheet();
-            Debug.Log(clueData.ToString());
+            
+            // Load from TextAsset text (uses default ES3Settings/encoding)
+            sheet.LoadRaw(clueData.text);
+            
+            Debug.Log(sheet.ToString());
+            Debug.Log("This is the row count: " + sheet.RowCount);
+            Debug.Log("This is the column count: " + sheet.ColumnCount);
+            
+            Debug.Log("Here is 1,1: " + sheet.GetCell<string>(1, 1));
+            
+            sheet.Save("myClueData.csv");
+            
+            
+            // // Save some values into the spreadsheet.
+            // sheet.SetCell(0,0, "hitpoints");
+            // sheet.SetCell(1,0, 12);
+            // sheet.SetCell(0,1, "position");
+            // sheet.SetCell(1,0, transform.position);
+            //
+            // // Save the file to a CSV.
+            // sheet.Save("myClueData.csv");
+            
             // sheet.Load(clueData.ToString());
+            //sheet.Load(clueData);
             // Debug.Log("Clue Spreadsheet Loaded: " + clueData.name);
             // Debug.Log("Here is a debug of the sheet to a to a string: " + sheet.ToString());
         }

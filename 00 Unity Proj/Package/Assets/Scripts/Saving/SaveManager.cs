2f16e2f5f6c83251b725e7f3591012535bdcae36
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-11-01 17:54:15 -0400

    SaveManager.cs Loads ProgressDataSpreadsheet
    Overview
    - Updated SaveManager.cs.
    
    In-depth Details
    - SaveManager.cs was created as a way to load spreadsheet data asynchronously from the cloud (27721fe).
    - I just updated the code and the remote load path URL so that now the progress data spreadsheet can also be loaded and saved to file.
    - Up next, I want to combine this system with the updated UI system in #73.

--- 00 Unity Proj/Package/Assets/Scripts/Saving/SaveManager.cs
+++ 00 Unity Proj/Package/Assets/Scripts/Saving/SaveManager.cs
@@ -12,10 +12,11 @@ public class SaveManager : MonoBehaviour
 
     private void Start()
     {
-        // Load the clueSpreadsheet asynchronously
-        // and register a callback for when it's loaded
+        // Load the spreadsheets asynchronously and register a callback upon completion
         clueSpreadsheet.LoadAssetAsync().Completed += OnClueSpreadsheetLoaded;
-        LoadSavedSpreadsheet();
+        LoadClueSpreadsheet();
+        progressSpreadsheet.LoadAssetAsync().Completed += OnProgressSpreadsheetLoaded;
+        LoadProgressSpreadsheet();
     }
 
     // When clueSpreadsheet is loaded, load the data into an ES3Spreadsheet
@@ -24,18 +25,12 @@ public class SaveManager : MonoBehaviour
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             TextAsset clueData = handle.Result;
-            // string assetPath = handle.Location?.InternalId ?? "unknown";
             var sheet = new ES3Spreadsheet();
             
             // Load from TextAsset text (uses default ES3Settings/encoding)
             sheet.LoadRaw(clueData.text);
-            
-            // Debug.Log(sheet.ToString());
-            // Debug.Log("This is the row count: " + sheet.RowCount);
-            // Debug.Log("This is the column count: " + sheet.ColumnCount);
-            //
-            // Debug.Log("Here is 1,1: " + sheet.GetCell<string>(1, 1));
-            
+
+            // Save the loaded spreadsheet to disk
             sheet.Save("myClueData.csv");
         }
         else
@@ -44,13 +39,36 @@ public class SaveManager : MonoBehaviour
         }
     }
     
-    public void LoadSavedSpreadsheet()
+    // When progressSpreadsheet is loaded, load the data into an ES3Spreadsheet
+    public void OnProgressSpreadsheetLoaded(AsyncOperationHandle<TextAsset> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            TextAsset progressData = handle.Result;
+            var sheet = new ES3Spreadsheet();
+            
+            // Load from TextAsset text (uses default ES3Settings/encoding)
+            sheet.LoadRaw(progressData.text);
+
+            // Save the loaded spreadsheet to disk
+            sheet.Save("myProgressData.csv");
+        }
+        else
+        {
+            Debug.LogError("Failed to load progress data spreadsheet.");
+        }
+    }
+    
+    // This will load a previously saved ES3Spreadsheet from disk
+    public void LoadClueSpreadsheet()
     {
         var sheet = new ES3Spreadsheet();
         sheet.Load("myClueData.csv");
-        // Debug.Log("Loaded saved spreadsheet:");
-        // Debug.Log(sheet.ToString());
-        // Debug.Log("LoadSavedSpreadsheet > This is the row count: " + sheet.RowCount);
-        // Debug.Log("LoadSavedSpreadsheet > This is the column count: " + sheet.ColumnCount);
+    }
+    
+    public void LoadProgressSpreadsheet()
+    {
+        var sheet = new ES3Spreadsheet();
+        sheet.Load("myProgressData.csv");
     }
 }

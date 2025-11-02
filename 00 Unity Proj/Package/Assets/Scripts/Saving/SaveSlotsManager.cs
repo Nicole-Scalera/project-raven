6732f503ab7f607c2de4a810bea5810a48af46c9
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-11-02 15:17:11 -0500

    Started Connecting Save Logic to Load Slots in MainMenu.unity
    Overview
    - Turned SaveManager.cs into SaveSlotsManager.cs.
    - Started integrating this script into MainMenu.unity.
    - Added "Save Slots Custom Directory - 11_2_25.mp4" to the 02 Docs folder.
    - This commit is related to #44.
    
    In-depth Details
    - SaveManager.cs was birthed out of a script named ClueSavingTest.cs (efaa13d).
    - It has now been renamed to SaveSlotsManager.cs.
         - I am currently crafting it to be utilized specifically in MainMenu.unity.
         - If I don't reuse it in other scenes, I will create a different SaveManager script.
    - Right now it is hooked up to the LoadSaves menu in MainMenu.unity (a5d5143).
    - It inherits from ES3SlotManager to actually save data to the Player's disk.
         - It overrides some methods, such as CreateNewSlot(), for custom logic.
    - I also made a method called RemoveWhitespaceSlotDirectory() that will create custom sub-directories whose names are based on the name of the save that the Player enters in the game.
         - This functionality overrides the hard-coded slotsDirectory string that saves all data in the same sub-folder.
         - I chose not to do this because I felt it would be better to distinguish logic into separate folders if we're working with separate saves.
    - For a demonstration, please check out the video titled "Save Slots Custom Directory - 11_2_25.mp4" that I am adding in this commit.

--- 00 Unity Proj/Package/Assets/Scripts/Saving/SaveSlotsManager.cs
+++ 00 Unity Proj/Package/Assets/Scripts/Saving/SaveSlotsManager.cs
@@ -1,22 +1,22 @@
+using System;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-// This class acts as a save manager to load and save important
-// data that should persist between play sessions.
+// This is the save slot manager. It inherits from ES3SlotManager.
 
-public class SaveManager : MonoBehaviour
+public class SaveSlotsManager : ES3SlotManager
 {
+    public string customSlotDirectory = "";
     [SerializeField] private AssetReferenceSpreadsheet clueSpreadsheet;
     [SerializeField] private AssetReferenceSpreadsheet progressSpreadsheet;
 
-    private void Start()
+    private void DownloadStarterData()
     {
         // Load the spreadsheets asynchronously and register a callback upon completion
         clueSpreadsheet.LoadAssetAsync().Completed += OnClueSpreadsheetLoaded;
-        // LoadClueSpreadsheet();
         progressSpreadsheet.LoadAssetAsync().Completed += OnProgressSpreadsheetLoaded;
-        // LoadProgressSpreadsheet();
     }
 
     // When clueSpreadsheet is loaded, load the data into an ES3Spreadsheet
@@ -29,9 +29,9 @@ public class SaveManager : MonoBehaviour
             
             // Load from TextAsset text (uses default ES3Settings/encoding)
             sheet.LoadRaw(clueData.text);
-
+    
             // Save the loaded spreadsheet to disk
-            sheet.Save($"slots/myClueData.csv");
+            sheet.Save($"{customSlotDirectory}/myClueData.csv");
         }
         else
         {
@@ -49,9 +49,9 @@ public class SaveManager : MonoBehaviour
             
             // Load from TextAsset text (uses default ES3Settings/encoding)
             sheet.LoadRaw(progressData.text);
-
+    
             // Save the loaded spreadsheet to disk
-            sheet.Save("slots/myProgressData.csv");
+            sheet.Save($"{customSlotDirectory}/myProgressData.csv");
         }
         else
         {
@@ -59,16 +59,43 @@ public class SaveManager : MonoBehaviour
         }
     }
     
-    // // This will load a previously saved ES3Spreadsheet from disk
-    // public void LoadClueSpreadsheet()
-    // {
-    //     var sheet = new ES3Spreadsheet();
-    //     sheet.Load("myClueData.csv");
-    // }
-    //
-    // public void LoadProgressSpreadsheet()
-    // {
-    //     var sheet = new ES3Spreadsheet();
-    //     sheet.Load("myProgressData.csv");
-    // }
+    public override ES3Slot CreateNewSlot(string slotName)
+    {
+        // Get the current timestamp.
+        var creationTimestamp = DateTime.Now;
+        
+        // Create the slot in the UI.
+        var slot = InstantiateSlot(slotName, creationTimestamp);
+        
+        // Move the slot to the top of the list.
+        slot.MoveToTop();
+        
+        ES3.SaveRaw("{123}", GetSlotPath(slotName));
+
+        Debug.Log("SaveSlotsManager > CreateNewSlot: Created new slot with name: " + slotName);
+        
+        // Create a sub-directory for this save based on its name
+        // and remove any whitespaces from the path
+        customSlotDirectory = RemoveWhitespaceSlotDirectory(slotName);
+        Debug.Log("SaveSlotsManager > RemoveWhitespaceSlotDirectory > customSlotDirectory is " + customSlotDirectory);
+
+        DownloadStarterData();
+        
+        // Select the slot if necessary.
+        if (selectSlotAfterCreation)
+            slot.SelectSlot();
+
+        // Scroll the scroll view to the top of the list.
+        ScrollToTop();
+
+        return slot;
+    }
+    
+    // Take any whitespace out of the slot directory name
+    public string RemoveWhitespaceSlotDirectory(string slotName)
+    {
+        // We convert any whitespace characters to underscores at this point to make the file more portable.
+        return Regex.Replace(slotName, @"\s+", "_");
+    }
+    
 }
